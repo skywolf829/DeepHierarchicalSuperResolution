bc6560a6dfc633003c4c12b1107e05cc7cf5865b
Skylar Wurster <swwurster@gmail.com>
2021-08-17 14:46:35 -0400

    Update run.sh

--- Code/ShellScripts/run.sh
+++ Code/ShellScripts/run.sh
@@ -36,7 +36,7 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--epochs 100 --min_dimension_size 16 \
 #--cropping_resolution 96
 
-python3 -u Code/train.py --save_name Isomag2D --train_distributed False --gpus_per_node 8 \
---num_workers 0 --data_folder Isomag2D --mode 2D --patch_size 1024 --training_patch_size 1024 \
---epochs 15 --min_dimension_size 16 \
---cropping_resolution 256 --alpha_2 0.1 
+python3 -u Code/train.py --save_name Isomag3D --train_distributed False --gpus_per_node 8 \
+--num_workers 0 --data_folder Isomag3D --mode 3D --patch_size 96 --training_patch_size 96 \
+--epochs 15 --min_dimension_size 64 \
+--cropping_resolution 96 --alpha_2 0.1 
