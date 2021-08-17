e91e728f419273058ea28628e6bdfcb83e674dc5
Skylar Wurster <swwurster@gmail.com>
2021-08-17 11:15:55 -0400

    Update run.sh

--- Code/ShellScripts/run.sh
+++ Code/ShellScripts/run.sh
@@ -31,7 +31,12 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--num_blocks 3 --base_num_kernels 96 --x_resolution 1024 --y_resolution 1024 --training_data_amount 0.02 --epochs 2500 \
 #--cropping_resolution 512
 
-python3 -u Code/train.py --save_name Mixing3D --train_distributed True --gpus_per_node 8 \
---num_workers 0 --data_folder Mixing3D --mode 3D --patch_size 96 --training_patch_size 96 \
---epochs 100 --min_dimension_size 16 \
---cropping_resolution 96
+#python3 -u Code/train.py --save_name Mixing3D --train_distributed True --gpus_per_node 8 \
+#--num_workers 0 --data_folder Mixing3D --mode 3D --patch_size 96 --training_patch_size 96 \
+#--epochs 100 --min_dimension_size 16 \
+#--cropping_resolution 96
+
+python3 -u Code/train.py --save_name Isomag2D --train_distributed False --gpus_per_node 8 \
+--num_workers 0 --data_folder Isomag2D --mode 2D --patch_size 1024 --training_patch_size 1024 \
+--epochs 100 --min_dimension_size 32 \
+--cropping_resolution 1024
