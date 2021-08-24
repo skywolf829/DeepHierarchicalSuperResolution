5b87232625ea19d15639bbbeb4c01278a5c78227
Skylar Wurster <swwurster@gmail.com>
2021-08-24 15:10:45 -0400

    Update run_tests.sh

--- Code/ShellScripts/run_tests.sh
+++ Code/ShellScripts/run_tests.sh
@@ -2,6 +2,6 @@
 cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 
 python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Isomag3D --model_name Isomag3D \
---device cuda:0 --parallel True --test_on_gpu True \
---output_file_name Isomag3D.results
+--mode 2D --data_folder Isomag2D --model_name Isomag2D \
+--device cuda:0 --parallel False --test_on_gpu True \
+--output_file_name Isomag2D.results
