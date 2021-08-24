598e24bfc586bc5ec5fe7ae91755ddaee854401e
Skylar Wurster <swwurster@gmail.com>
2021-08-24 12:30:41 -0400

    Update run.sh

--- Code/ShellScripts/run.sh
+++ Code/ShellScripts/run.sh
@@ -5,7 +5,7 @@ python3 -u Code/train.py --save_name Isomag2D --train_distributed True \
 --beta_1 0.9 --beta_2 0.999 \
 --num_workers 0 --data_folder Isomag2D --mode 2D \
 --cropping_resolution 256 --patch_size 1024 --training_patch_size 1024 \
---num_blocks 3 --base_num_kernels 96 --epochs 50 --random_flipping True \
+--num_blocks 3 --epochs 50 --random_flipping True \
 --min_dimension_size 16
 
 #python3 -u Code/train.py --save_name Mixing3D --train_distributed True --gpus_per_node 8 \
