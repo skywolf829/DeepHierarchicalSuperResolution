2a843f41484ec1cf0fe18f30dcfce01e76c90974
Skylar Wurster <swwurster@gmail.com>
2021-08-26 09:15:42 -0400

    Update run_compression_tests.sh

--- Code/ShellScripts/run_compression_tests.sh
+++ Code/ShellScripts/run_compression_tests.sh
@@ -20,31 +20,31 @@ export PATH="$HOME/tthresh/build:$PATH"
 #--save_netcdf false --device cuda:0
 
 # 3D iso1024 mag
-#python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
-#--upscaling_technique model --model_name Isomag3D --criterion psnr --start_metric 25 \
-#--end_metric 60 --metric_skip 1.0 --output_folder Isomag3D_datareduction --max_LOD 4 \
-#--min_chunk 16 --mode 3D --file Isomag3D.h5 --dims 3 --nx 1024 --ny 1024 --nz 1024 \
-#--use_compressor true --distributed True --compressor sz --load_existing false \
-#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
-#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
+--upscaling_technique model --model_name Isomag3D --criterion psnr --start_metric 25 \
+--end_metric 60 --metric_skip 1.0 --output_folder Isomag3D_datareduction --max_LOD 4 \
+--min_chunk 16 --mode 3D --file Isomag3D.h5 --dims 3 --nx 1024 --ny 1024 --nz 1024 \
+--use_compressor true --distributed True --compressor sz --load_existing false \
+--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-#python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
-#--upscaling_technique model --model_name Isomag3D --criterion psnr --start_metric 25 \
-#--end_metric 60 --metric_skip 1.0 --output_folder Isomag3D_datareduction --max_LOD 4 \
-#--min_chunk 16 --mode 3D --file Isomag3D.h5 --dims 3 --nx 1024 --ny 1024 --nz 1024 \
-#--use_compressor true --distributed True --compressor tthresh --load_existing false \
-#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
-#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
+--upscaling_technique model --model_name Isomag3D --criterion psnr --start_metric 25 \
+--end_metric 60 --metric_skip 1.0 --output_folder Isomag3D_datareduction --max_LOD 4 \
+--min_chunk 16 --mode 3D --file Isomag3D.h5 --dims 3 --nx 1024 --ny 1024 --nz 1024 \
+--use_compressor true --distributed True --compressor tthresh --load_existing false \
+--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-#python3 -u Code/TestingScripts/sz_test.py --file Isomag3D.h5 \
-#--dims 3 --nx 1024 --ny 1024 --nz 1024 --output_folder Isomag3D_datareduction \
-#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
-#--save_netcdf false --device cpu
+python3 -u Code/TestingScripts/sz_test.py --file Isomag3D.h5 \
+--dims 3 --nx 1024 --ny 1024 --nz 1024 --output_folder Isomag3D_datareduction \
+--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+--save_netcdf false --device cpu
 
-#python3 -u Code/TestingScripts/tthresh_test.py --file Isomag3D.h5 \
-#--dims 3 --nx 1024 --ny 1024 --nz 1024 --output_folder Isomag3D_datareduction \
-#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
-#--save_netcdf false --device cpu
+python3 -u Code/TestingScripts/tthresh_test.py --file Isomag3D.h5 \
+--dims 3 --nx 1024 --ny 1024 --nz 1024 --output_folder Isomag3D_datareduction \
+--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+--save_netcdf false --device cpu
 
 # 3D mixing dataset
 #python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool2D \
@@ -65,57 +65,57 @@ export PATH="$HOME/tthresh/build:$PATH"
 
 
 # Plume dataset
-python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Plume --criterion psnr --start_metric 25 \
---end_metric 60 --metric_skip 1.0 --output_folder Plume_datareduction --max_LOD 3 \
---min_chunk 16 --mode 3D --file Plume.h5 --dims 3 --nx 512 --ny 512 --nz 128 \
---use_compressor true --distributed false --compressor tthresh --load_existing false \
---save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
---device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+#python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Plume --criterion psnr --start_metric 25 \
+#--end_metric 60 --metric_skip 1.0 --output_folder Plume_datareduction --max_LOD 3 \
+#--min_chunk 16 --mode 3D --file Plume.h5 --dims 3 --nx 512 --ny 512 --nz 128 \
+#--use_compressor true --distributed false --compressor tthresh --load_existing false \
+#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Plume --criterion psnr --start_metric 25 \
---end_metric 60 --metric_skip 1.0 --output_folder Plume_datareduction --max_LOD 3 \
---min_chunk 16 --mode 3D --file Plume.h5 --dims 3 --nx 512 --ny 512 --nz 128 \
---use_compressor true --distributed false --compressor sz --load_existing false \
---save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
---device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+#python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Plume --criterion psnr --start_metric 25 \
+#--end_metric 60 --metric_skip 1.0 --output_folder Plume_datareduction --max_LOD 3 \
+#--min_chunk 16 --mode 3D --file Plume.h5 --dims 3 --nx 512 --ny 512 --nz 128 \
+#--use_compressor true --distributed false --compressor sz --load_existing false \
+#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-python3 -u Code/TestingScripts/sz_test.py --file Plume.h5 \
---dims 3 --nx 512 --ny 512 --nz 128 --output_folder Plume_datareduction \
---start_value 10 --end_value 60 --value_skip 1 --metric psnr \
---save_netcdf false --device cuda:0
+#python3 -u Code/TestingScripts/sz_test.py --file Plume.h5 \
+#--dims 3 --nx 512 --ny 512 --nz 128 --output_folder Plume_datareduction \
+#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+#--save_netcdf false --device cuda:0
 
-python3 -u Code/TestingScripts/tthresh_test.py --file Plume.h5 \
---dims 3 --nx 512 --ny 512 --nz 128 --output_folder Plume_datareduction \
---start_value 10 --end_value 60 --value_skip 1 --metric psnr \
---save_netcdf false --device cuda:0
+#python3 -u Code/TestingScripts/tthresh_test.py --file Plume.h5 \
+#--dims 3 --nx 512 --ny 512 --nz 128 --output_folder Plume_datareduction \
+#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+#--save_netcdf false --device cuda:0
 
 # Vorts dataset
-python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Vorts --criterion psnr --start_metric 25 \
---end_metric 60 --metric_skip 1.0 --output_folder Vorts_datareduction --max_LOD 3 \
---min_chunk 16 --mode 3D --file Vorts.h5 --dims 3 --nx 128 --ny 128 --nz 128 \
---use_compressor true --distributed false --compressor tthresh --load_existing false \
---save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
---device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+#python3 -u Code/mixedLOD_octree.py --save_name NN_TTHRESH --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Vorts --criterion psnr --start_metric 25 \
+#--end_metric 60 --metric_skip 1.0 --output_folder Vorts_datareduction --max_LOD 3 \
+#--min_chunk 16 --mode 3D --file Vorts.h5 --dims 3 --nx 128 --ny 128 --nz 128 \
+#--use_compressor true --distributed false --compressor tthresh --load_existing false \
+#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
---upscaling_technique model --model_name Vorts --criterion psnr --start_metric 25 \
---end_metric 60 --metric_skip 1.0 --output_folder Vorts_datareduction --max_LOD 3 \
---min_chunk 16 --mode 3D --file Vorts.h5 --dims 3 --nx 128 --ny 128 --nz 128 \
---use_compressor true --distributed false --compressor sz --load_existing false \
---save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
---device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
+#python3 -u Code/mixedLOD_octree.py --save_name NN_SZ --downscaling_technique avgpool3D \
+#--upscaling_technique model --model_name Vorts --criterion psnr --start_metric 25 \
+#--end_metric 60 --metric_skip 1.0 --output_folder Vorts_datareduction --max_LOD 3 \
+#--min_chunk 16 --mode 3D --file Vorts.h5 --dims 3 --nx 128 --ny 128 --nz 128 \
+#--use_compressor true --distributed false --compressor sz --load_existing false \
+#--save_netcdf false --save_netcdf_octree false --debug true --preupscaling_PSNR true \
+#--device cuda:0 --dynamic_downscaling true --interpolation_heuristic true 
 
-python3 -u Code/TestingScripts/sz_test.py --file Vorts.h5 \
---dims 3 --nx 128 --ny 128 --nz 128 --output_folder Vorts_datareduction \
---start_value 10 --end_value 60 --value_skip 1 --metric psnr \
---save_netcdf false --device cuda:0
+#python3 -u Code/TestingScripts/sz_test.py --file Vorts.h5 \
+#--dims 3 --nx 128 --ny 128 --nz 128 --output_folder Vorts_datareduction \
+#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+#--save_netcdf false --device cuda:0
 
-python3 -u Code/TestingScripts/tthresh_test.py --file Vorts.h5 \
---dims 3 --nx 128 --ny 128 --nz 128 --output_folder Vorts_datareduction \
---start_value 10 --end_value 60 --value_skip 1 --metric psnr \
---save_netcdf false --device cuda:0
+#python3 -u Code/TestingScripts/tthresh_test.py --file Vorts.h5 \
+#--dims 3 --nx 128 --ny 128 --nz 128 --output_folder Vorts_datareduction \
+#--start_value 10 --end_value 60 --value_skip 1 --metric psnr \
+#--save_netcdf false --device cuda:0
 
 
