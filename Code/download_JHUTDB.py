e8f466d760510d2617cd9651956ba20bdc274399
Skylar Wurster <swwurster@gmail.com>
2021-09-13 11:35:57 -0400

    Update download_JHUTDB.py

--- Code/download_JHUTDB.py
+++ Code/download_JHUTDB.py
@@ -115,7 +115,7 @@ for i in range(startts, endts, ts_skip):
     print(f.shape)
     f = np.expand_dims(f, 0)
     f -= f.min()
-    f *= 1/f.max()
+    f *= 1/(f.max() + 1e-6)
     print(f.shape)
     #frames.append(f)
     f_h5 = h5py.File(os.path.join(save_folder, str(i-1)+ '.h5'), 'w')
