a0599baf77f2d374ab86d52adc23a1ba09649bdf
Skylar Wurster <swwurster@gmail.com>
2021-09-14 19:17:45 -0400

    Update run_tests.sh

--- Code/ShellScripts/run_tests.sh
+++ Code/ShellScripts/run_tests.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 2D --data_folder Isomag2D --model_name Isomag2D \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Isomag2D.results --dict_entry_name model
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 2D --data_folder Isomag2D --model_name Isomag2D \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Isomag2D.results --dict_entry_name model
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 2D --data_folder Isomag2D --model_name Isomag2D_noGAN \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Isomag2D.results --dict_entry_name model_noGAN
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 2D --data_folder Isomag2D --model_name Isomag2D_noGAN \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Isomag2D.results --dict_entry_name model_noGAN
 
 python3 -u Code/TestingScripts/test_SSR.py \
 --mode 3D --data_folder Isomag3D --model_name Isomag3D \
@@ -21,34 +21,34 @@ python3 -u Code/TestingScripts/test_SSR.py \
 --device cuda:0 --parallel True --test_on_gpu True \
 --output_file_name Isomag3D.results --dict_entry_name model_noGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Mixing3D --model_name Mixing3D \
---device cuda:0 --parallel True --test_on_gpu True \
---output_file_name Mixing3D.results --dict_entry_name model
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Mixing3D --model_name Mixing3D \
+#--device cuda:0 --parallel True --test_on_gpu True \
+#--output_file_name Mixing3D.results --dict_entry_name model
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Mixing3D --model_name Mixing3D_noGAN \
---device cuda:0 --parallel True --test_on_gpu True \
---output_file_name Mixing3D.results --dict_entry_name model_noGAN
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Mixing3D --model_name Mixing3D_noGAN \
+#--device cuda:0 --parallel True --test_on_gpu True \
+#--output_file_name Mixing3D.results --dict_entry_name model_noGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Plume --model_name Plume \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Plume.results --dict_entry_name model
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Plume --model_name Plume \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Plume.results --dict_entry_name model
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Plume --model_name Plume_noGAN \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Plume.results --dict_entry_name model_noGAN
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Plume --model_name Plume_noGAN \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Plume.results --dict_entry_name model_noGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Vorts --model_name Vorts \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Vorts.results --dict_entry_name model
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Vorts --model_name Vorts \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Vorts.results --dict_entry_name model
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Vorts --model_name Vorts_noGAN \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Vorts.results --dict_entry_name model_noGAN
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Vorts --model_name Vorts_noGAN \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Vorts.results --dict_entry_name model_noGAN
 
 
