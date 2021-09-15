054888111a4b2ca6e686e538a66c38efcde4157e
Skylar Wurster <swwurster@gmail.com>
2021-09-15 14:37:39 -0400

    Changes run script for eval

--- Code/ShellScripts/run_tests.sh
+++ Code/ShellScripts/run_tests.sh
@@ -11,25 +11,25 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--device cuda:0 --parallel False --test_on_gpu True \
 #--output_file_name Isomag2D.results --dict_entry_name model_noGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Isomag3D --model_name Isomag3D \
---device cuda:0 --parallel True --test_on_gpu True \
---output_file_name Isomag3D.results --dict_entry_name model
-
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Isomag3D --model_name Isomag3D_noGAN \
---device cuda:0 --parallel True --test_on_gpu True \
---output_file_name Isomag3D.results --dict_entry_name model_noGAN
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Isomag3D --model_name Isomag3D \
+#--device cuda:0 --parallel True --test_on_gpu True \
+#--output_file_name Isomag3D.results --dict_entry_name model
 
 #python3 -u Code/TestingScripts/test_SSR.py \
-#--mode 3D --data_folder Mixing3D --model_name Mixing3D \
+#--mode 3D --data_folder Isomag3D --model_name Isomag3D_noGAN \
 #--device cuda:0 --parallel True --test_on_gpu True \
-#--output_file_name Mixing3D.results --dict_entry_name model
+#--output_file_name Isomag3D.results --dict_entry_name model_noGAN
 
-#python3 -u Code/TestingScripts/test_SSR.py \
-#--mode 3D --data_folder Mixing3D --model_name Mixing3D_noGAN \
-#--device cuda:0 --parallel True --test_on_gpu True \
-#--output_file_name Mixing3D.results --dict_entry_name model_noGAN
+python3 -u Code/TestingScripts/test_SSR.py \
+--mode 3D --data_folder Mixing3D --model_name Mixing3D \
+--device cuda:0 --parallel True --test_on_gpu True \
+--output_file_name Mixing3D.results --dict_entry_name model
+
+python3 -u Code/TestingScripts/test_SSR.py \
+--mode 3D --data_folder Mixing3D --model_name Mixing3D_noGAN \
+--device cuda:0 --parallel True --test_on_gpu True \
+--output_file_name Mixing3D.results --dict_entry_name model_noGAN
 
 #python3 -u Code/TestingScripts/test_SSR.py \
 #--mode 3D --data_folder Plume --model_name Plume \
