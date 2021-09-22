214a9c3ed3aac079c49bdbf48fdbdad680a307e8
Skylar Wurster <swwurster@gmail.com>
2021-09-22 19:36:12 -0400

    Update train_ESRGANs.sh

--- Code/ShellScripts/train_ESRGANs.sh
+++ Code/ShellScripts/train_ESRGANs.sh
@@ -10,13 +10,13 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--alpha_1 1.0 --alpha_2 0.00 \
 #--model ESRGAN --generator_steps 1 
 
-python3 -u Code/train.py --save_name Isomag3D_ESRGAN --train_distributed True \
---beta_1 0.9 --beta_2 0.999 \
---num_workers 0 --data_folder Isomag3D --mode 3D \
---cropping_resolution 96 --patch_size 96 --training_patch_size 96 \
---num_blocks 3 --epochs 100 --random_flipping True \
---min_dimension_size 32 --g_lr 0.0002 --alpha_1 1.0 --alpha_2 0.00 \
---model ESRGAN --generator_steps 1
+#python3 -u Code/train.py --save_name Isomag3D_ESRGAN --train_distributed True \
+#--beta_1 0.9 --beta_2 0.999 \
+#--num_workers 0 --data_folder Isomag3D --mode 3D \
+#--cropping_resolution 96 --patch_size 96 --training_patch_size 96 \
+#--num_blocks 3 --epochs 100 --random_flipping True \
+#--min_dimension_size 32 --g_lr 0.0002 --alpha_1 1.0 --alpha_2 0.00 \
+#--model ESRGAN --generator_steps 1
 
 python3 -u Code/train.py --save_name Mixing3D_ESRGAN --train_distributed True --gpus_per_node 8 \
 --num_workers 0 --data_folder Mixing3D --mode 3D --patch_size 96 --training_patch_size 96 \
@@ -26,12 +26,12 @@ python3 -u Code/train.py --save_name Mixing3D_ESRGAN --train_distributed True --
 
 python3 -u Code/train.py --save_name Plume_ESRGAN --train_distributed True --gpus_per_node 8 \
 --num_workers 0 --data_folder Plume --mode 3D --patch_size 96 --training_patch_size 96 \
---epochs 250 --min_dimension_size 32 --cropping_resolution 96 --g_lr 0.0002 \
+--epochs 250 --min_dimension_size 32 --cropping_resolution 96 --g_lr 0.00002 \
 --alpha_1 1.0 --alpha_2 0.00 \
 --model ESRGAN --generator_steps 1
 
 python3 -u Code/train.py --save_name Vorts_ESRGAN --train_distributed True --gpus_per_node 8 \
 --num_workers 0 --data_folder Vorts --mode 3D --patch_size 96 --training_patch_size 96 \
---epochs 250 --min_dimension_size 32 --cropping_resolution 96 --g_lr 0.0002 \
+--epochs 250 --min_dimension_size 32 --cropping_resolution 96 --g_lr 0.00002 \
 --alpha_1 1.0 --alpha_2 0.00 \
 --model ESRGAN --generator_steps 1
