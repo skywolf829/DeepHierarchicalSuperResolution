268f50ccb898f00eab5074987d19fc86d073aade
Skylar Wurster <swwurster@gmail.com>
2021-09-23 10:13:30 -0400

    Update models.py

--- Code/models.py
+++ Code/models.py
@@ -394,7 +394,7 @@ class SSRTVD_G_2x(nn.Module):
         self.IB3 = IB(opt, 64+16, 128)
 
        
-        self.deconv = nn.ConvTranspose3d(128, 32, 4, 1) if opt['mode'] == "3D" else nn.ConvTranspose2d(128, 128, 4, 1)
+        self.deconv = nn.ConvTranspose3d(128, 32, 4, 1) if opt['mode'] == "3D" else nn.ConvTranspose2d(128, 32, 4, 1)
         self.deconv_IB1 = IB(opt, 32, 8)
         self.deconv_IB2 = IB(opt, 8+32, 1)
 
