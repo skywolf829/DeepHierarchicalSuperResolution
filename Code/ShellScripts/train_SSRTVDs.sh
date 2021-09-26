13ee069d104ef9a2118873d206579891d099e4ee
Skylar Wurster <swwurster@gmail.com>
2021-09-26 12:39:43 -0400

    Update train_SSRTVDs.sh

--- Code/ShellScripts/train_SSRTVDs.sh
+++ Code/ShellScripts/train_SSRTVDs.sh
@@ -10,20 +10,20 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--alpha_1 1.0 --alpha_2 0.001 --d_lr 0.0004 \
 #--model SSRTVD --generator_steps 1 --discriminator_steps 2
 
-python3 -u Code/train.py --save_name Isomag3D_SSRTVD --train_distributed False \
---beta_1 0.5 --beta_2 0.999 \
---num_workers 0 --data_folder Isomag3D --mode 3D \
---cropping_resolution 96 --patch_size 96 --training_patch_size 96 \
---epochs 15 --random_flipping True \
---min_dimension_size 32 --g_lr 0.0001 --d_lr 0.0004 --alpha_1 1.0 --alpha_2 0.001 \
---model SSRTVD --generator_steps 1 --discriminator_steps 2
+#python3 -u Code/train.py --save_name Isomag3D_SSRTVD --train_distributed False \
+#--beta_1 0.5 --beta_2 0.999 \
+#--num_workers 0 --data_folder Isomag3D --mode 3D \
+#--cropping_resolution 96 --patch_size 96 --training_patch_size 96 \
+#--epochs 15 --random_flipping True \
+#--min_dimension_size 32 --g_lr 0.0001 --d_lr 0.0004 --alpha_1 1.0 --alpha_2 0.001 \
+#--model SSRTVD --generator_steps 1 --discriminator_steps 2
 
-python3 -u Code/train.py --save_name Mixing3D_SSRTVD --train_distributed False --gpus_per_node 8 \
---num_workers 0 --data_folder Mixing3D --mode 3D --patch_size 96 --training_patch_size 96 \
---beta_1 0.5 --beta_2 0.999 \
---epochs 20 --cropping_resolution 96 --min_dimension_size 32 \
---g_lr 0.0001 --d_lr 0.0004 --alpha_1 1.0 --alpha_2 0.001 \
---model SSRTVD --generator_steps 1 --discriminator_steps 2
+#python3 -u Code/train.py --save_name Mixing3D_SSRTVD --train_distributed False --gpus_per_node 8 \
+#--num_workers 0 --data_folder Mixing3D --mode 3D --patch_size 96 --training_patch_size 96 \
+#--beta_1 0.5 --beta_2 0.999 \
+#--epochs 20 --cropping_resolution 96 --min_dimension_size 32 \
+#--g_lr 0.0001 --d_lr 0.0004 --alpha_1 1.0 --alpha_2 0.001 \
+#--model SSRTVD --generator_steps 1 --discriminator_steps 2
 
 python3 -u Code/train.py --save_name Plume_SSRTVD --train_distributed False --gpus_per_node 8 \
 --num_workers 0 --data_folder Plume --mode 3D --patch_size 96 --training_patch_size 96 \
