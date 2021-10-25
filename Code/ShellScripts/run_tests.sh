4e1d2e32e342a46eaf202a5b1b4897e902f5a1a1
Skylar Wurster <swwurster@gmail.com>
2021-10-25 13:40:38 -0400

    Adds supernova training code

--- Code/ShellScripts/run_tests.sh
+++ Code/ShellScripts/run_tests.sh
@@ -36,19 +36,28 @@
 #--device cuda:0 --parallel False --test_on_gpu True \
 #--output_file_name Plume.results --dict_entry_name ESRGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Plume --model_name Plume_SSRTVD \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Plume.results --dict_entry_name SSRTVD
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Plume --model_name Plume_SSRTVD \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Plume.results --dict_entry_name SSRTVD
 
 #python3 -u Code/TestingScripts/test_SSR.py \
 #--mode 3D --data_folder Vorts --model_name Vorts_ESRGAN \
 #--device cuda:0 --parallel False --test_on_gpu True \
 #--output_file_name Vorts.results --dict_entry_name ESRGAN
 
-python3 -u Code/TestingScripts/test_SSR.py \
---mode 3D --data_folder Vorts --model_name Vorts_SSRTVD \
---device cuda:0 --parallel False --test_on_gpu True \
---output_file_name Vorts.results --dict_entry_name SSRTVD
+#python3 -u Code/TestingScripts/test_SSR.py \
+#--mode 3D --data_folder Vorts --model_name Vorts_SSRTVD \
+#--device cuda:0 --parallel False --test_on_gpu True \
+#--output_file_name Vorts.results --dict_entry_name SSRTVD
 
+python3 -u Code/TestingScripts/test_SSR.py \
+--mode 3D --data_folder Supernova --model_name Supernova_ESRGAN \
+--device cuda:0 --parallel True --test_on_gpu True \
+--output_file_name Supernova.results --dict_entry_name ESRGAN
+
+python3 -u Code/TestingScripts/test_SSR.py \
+--mode 3D --data_folder Supernova --model_name Supernova_SSRTVD \
+--device cuda:0 --parallel True --test_on_gpu True \
+--output_file_name Supernova.results --dict_entry_name SSRTVD
 
--- Code/ShellScripts/train_SSRTVDs.sh
+++ Code/ShellScripts/train_SSRTVDs.sh
@@ -25,16 +25,23 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--g_lr 0.0001 --d_lr 0.0004 --alpha_1 1.0 --alpha_2 0.001 \
 #--model SSRTVD --generator_steps 1 --discriminator_steps 2
 
-python3 -u Code/train.py --save_name Plume_SSRTVD --train_distributed False --gpus_per_node 8 \
---num_workers 0 --data_folder Plume --mode 3D --patch_size 64 --training_patch_size 64 \
---beta_1 0.5 --beta_2 0.999 \
---epochs 100 --min_dimension_size 32 --cropping_resolution 64 --g_lr 0.0001 \
---alpha_1 1.0 --alpha_2 0.001 --d_lr 0.0004 \
---model SSRTVD --generator_steps 1 --discriminator_steps 2
+#python3 -u Code/train.py --save_name Plume_SSRTVD --train_distributed False --gpus_per_node 8 \
+#--num_workers 0 --data_folder Plume --mode 3D --patch_size 64 --training_patch_size 64 \
+#--beta_1 0.5 --beta_2 0.999 \
+#--epochs 100 --min_dimension_size 32 --cropping_resolution 64 --g_lr 0.0001 \
+#--alpha_1 1.0 --alpha_2 0.001 --d_lr 0.0004 \
+#--model SSRTVD --generator_steps 1 --discriminator_steps 2
 
-python3 -u Code/train.py --save_name Vorts_SSRTVD --train_distributed False --gpus_per_node 8 \
+#python3 -u Code/train.py --save_name Vorts_SSRTVD --train_distributed False --gpus_per_node 8 \
+#--beta_1 0.5 --beta_2 0.999 \
+#--num_workers 0 --data_folder Vorts --mode 3D --patch_size 64 --training_patch_size 64 \
+#--epochs 100 --min_dimension_size 32 --cropping_resolution 64 --g_lr 0.0001 \
+#--alpha_1 1.0 --alpha_2 0.001 --d_lr 0.0004 \
+#--model SSRTVD --generator_steps 1 --discriminator_steps 2
+
+python3 -u Code/train.py --save_name Supernova_SSRTVD --train_distributed False --gpus_per_node 8 \
 --beta_1 0.5 --beta_2 0.999 \
---num_workers 0 --data_folder Vorts --mode 3D --patch_size 64 --training_patch_size 64 \
+--num_workers 0 --data_folder Supernova --mode 3D --patch_size 64 --training_patch_size 64 \
 --epochs 100 --min_dimension_size 32 --cropping_resolution 64 --g_lr 0.0001 \
 --alpha_1 1.0 --alpha_2 0.001 --d_lr 0.0004 \
 --model SSRTVD --generator_steps 1 --discriminator_steps 2
