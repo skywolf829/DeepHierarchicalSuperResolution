b99b6e4497c444d4f6cfc5c78e124d209ab0c64f
Skylar Wurster <swwurster@gmail.com>
2022-05-05 10:10:29 -0400

    Adds arguments for T, h, skip

--- Code/FTLE.py
+++ Code/FTLE.py
@@ -2,20 +2,18 @@ from __future__ import absolute_import, division, print_function
 import argparse
 import time
 import os
-
 import matplotlib
 from options import *
 from netCDF4 import Dataset
 import numpy as np
 import imageio
-from scipy.interpolate import RegularGridInterpolator  
 import matplotlib.pyplot as plt
 from numba import njit
 
 @njit
 def RK4(vf, positions, t0, h=0.5, direction="forward"):
     
-    s = (int(positions.shape[0]), 1)
+    s = (positions.shape[0], 1)
     ones = np.ones(s)*t0
 
     k1 = interpolate(vf, positions)
@@ -143,40 +141,20 @@ def solution_at(data, positions):
 
     return solutions
 
-class interpolator_3D():
-    def __init__(self, u_interpolator, v_interpolator):
-        self.u_interpolator = u_interpolator
-        self.v_interpolator = v_interpolator
-
-    def __call__(self, points):
-        u_values = self.u_interpolator(points)
-        v_values = self.v_interpolator(points)
-        interp_values = np.stack([v_values, u_values], axis=1)
-        return interp_values
-
 @njit
-def particle_tracer(vf, positions, tstart = 0, tmax=100, h=0.5):
-    yg = np.linspace(0, vf.shape[2]-1, vf.shape[2])
-    xg = np.linspace(0, vf.shape[3]-1, vf.shape[3])
-    tg = np.linspace(0, vf.shape[0]-1, vf.shape[0])
-      
-    u_interpolator = RegularGridInterpolator((tg,yg,xg), vf[:,1], 
-                                             bounds_error=False, 
-                                             fill_value=0)
-    v_interpolator = RegularGridInterpolator((tg,yg,xg), vf[:,0], 
-                                             bounds_error=False, 
-                                             fill_value=0)
-    interp = interpolator_3D(u_interpolator, v_interpolator)
-
+def particle_tracer(vf, positions, tstart = 0.0, tfinish = 100.0, h = 0.5):
     traces = []
     traces.append(positions.copy())
     
-    for t in range(tstart, tstart+int(tmax / h)):
-        t0 = t*h
-        positions = RK4(
-            np.concatenate([np.ones([positions.shape[0], 1])*t0,positions], axis=1), 
-            interp, t0, h, "forward")[:,1:]
-        traces.append(positions)
+    ts = np.arange(tstart, tfinish-h, h)
+    for i in range(len(ts)):
+        t = ts[i]
+        s = (int(positions.shape[0]), 1)
+        current_time = np.ones(s)*t
+        interp_spots = np.concatenate((current_time,positions), axis=1)
+        positions = RK4(vf,interp_spots, 
+            t, h, "forward")[:,1:]
+        traces.append(positions.copy())
         
     return traces
 
@@ -196,29 +174,20 @@ def vf_to_flow_map(vf, t0, T, h=0.5, direction="forward"):
     yg = np.linspace(0, vf.shape[1]-1, vf.shape[1])
     xg = np.linspace(0, vf.shape[2]-1, vf.shape[2])
     g = np.stack(meshgrid(yg,xg))
-    '''
-    u_interpolator = RegularGridInterpolator((tg,yg,xg), vf[:,1], 
-                                             bounds_error=False, 
-                                             fill_value=0)
-    v_interpolator = RegularGridInterpolator((tg,yg,xg), vf[:,0], 
-                                             bounds_error=False, 
-                                             fill_value=0)
-    interp = interpolator_3D(u_interpolator, v_interpolator)
-    '''
-
+    
     t = t0    
-    tmax = min(t0+T, vf.shape[0])
+    tmax = max(min(t0+T, vf.shape[0]), 0)
 
     positions = np.ascontiguousarray(g.copy().transpose((1,2,0))).reshape(-1, 2)
-    while(t < tmax):
-        #print(f"Tracing flow map at time {t}/{tmax}")
+    ts = np.arange(t0, tmax-h, h)
+    for i in range(len(ts)):
+        t = ts[i] 
         s = (int(positions.shape[0]), 1)
         current_time = np.ones(s)*t
         interp_spots = np.concatenate((current_time,positions), axis=1)
         positions = RK4(
             vf, interp_spots,
-            t0, h, "forward")[:,1:]
-        t += h
+            t, h, "forward")[:,1:]
     flow_map = np.ascontiguousarray(positions.transpose((1,0))).reshape(g.shape)
     return flow_map
 
@@ -257,6 +226,7 @@ def vf_to_gif(vf, save_name):
     mag /= mag.max()
     mag *= 255
     mag = mag.astype(np.uint8)
+    mag = np.flip(mag, axis=1)
     
     imageio.mimsave(save_name + ".gif", mag, fps=60)
 
@@ -265,6 +235,7 @@ def ftle_to_gif(ftle, save_name):
     ftle /= (ftle.max()+1e-8)
     ftle *= 255
     ftle = ftle.astype(np.uint8)
+    ftle = np.flip(ftle, axis=1)
     imageio.mimsave(save_name + ".gif", ftle, fps=60)
 
 if __name__ == '__main__':
@@ -272,6 +243,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculate and save FTLE for data')
     parser.add_argument('--data_name',default=None,type=str,help='Input file')
     parser.add_argument('--save_name',default=None,type=str,help='Save folder name')
+    parser.add_argument('--T',default=10,type=float,help='Advection time')
+    parser.add_argument('--h',default=0.5,type=float,help='Advection step')
+    parser.add_argument('--skip',default=1,type=int,help='timestep skip')
     
     args = vars(parser.parse_args())
 
@@ -293,24 +267,23 @@ if __name__ == '__main__':
     vf = np.transpose(vf, (1, 2, 3, 0))    
     print(vf.shape)
 
-    vf_to_gif(vf[0:100], args['save_name'])
+    vf_to_gif(vf[0:1000], args['save_name'])
     
     # Make the vf in computation space
-    vf[...,0] *= (vf.shape[1] / (0.5 + 0.5))
-    vf[...,1] *= (vf.shape[2] / (7.5 + 0.5))
-    vf *= (15 / vf.shape[0])
+    vf[...,0] *= (vf.shape[1] / (3.0))
+    vf[...,1] *= (vf.shape[2] / (1.0))
+    vf *= (20.0 / vf.shape[0])
     
-    '''
-    # Check particle tracer
+    '''# Check particle tracer
     traces = particle_tracer(vf, 
                              np.array(
-                                 [[30, 5], 
-                                  [50, 5],
-                                  [40, 5],
-                                  [39, 5],
-                                  [37, 5]]),
-                             tmax=1500,
-                             tstart=600,
+                                 [[30.0, 5.0], 
+                                  [50.0, 5.0],
+                                  [40.0, 5.0],
+                                  [39.0, 5.0],
+                                  [37.0, 5.0]]),
+                             tstart=000.0,
+                             tfinish=1500.0,
                              h=0.5)
     traces = np.stack(traces)
     print(traces.shape)
@@ -324,18 +297,17 @@ if __name__ == '__main__':
     c = matplotlib.collections.PatchCollection([circle], color='black')
     ax.add_collection(c)
     plt.title("Particle trace")
-    plt.xlim([0, vf.shape[3]])
+    plt.xlim([0, vf.shape[2]])
     plt.xlabel("x")
-    plt.ylim([0, vf.shape[2]])
+    plt.ylim([0, vf.shape[1]])
     plt.ylabel("y")
-    plt.show()    
-    '''
+    plt.show()'''    
     
-    T = 50
-    h = 0.5
-    skip = 25
+    T = args['T']
+    h = args['H']
+    skip = args['skip']
     flow_maps = []
-    for t0 in range(0, vf.shape[0]-T, skip):
+    for t0 in np.arange(0.0, vf.shape[0]-T, skip):
         print(f"Calculting flow map {t0}/{vf.shape[0]}")
         t_start = time.time()
         fm = vf_to_flow_map(vf, t0, T, h)
@@ -343,10 +315,8 @@ if __name__ == '__main__':
         t_passed = t_end - t_start
         print(f"Calculation took {t_passed : 0.02f} seconds")
         flow_maps.append(fm)
-    
     flow_maps = np.stack(flow_maps)
     
     ftle = FTLE_from_flow_map(flow_maps, T)
-
-    ftle_to_gif(ftle, args['save_name']+"_ftle")
+    ftle_to_gif(ftle[0:1000], args['save_name']+"_ftle")
     
