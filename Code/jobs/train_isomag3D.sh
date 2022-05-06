40abb2e07f2dd8dc6f84b1e570ac7bdd51b82714
Skylar Wurster <swwurster@gmail.com>
2022-05-06 07:36:58 -0400

    Update train_isomag3D.sh

--- Code/jobs/train_isomag3D.sh
+++ Code/jobs/train_isomag3D.sh
@@ -12,8 +12,8 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 
 python -u Code/train.py \
 --train_distributed true --gpus_per_node 8 \
---mode 3D --model SSRTVD_NO_G \
---data_folder Isomag3D --save_name isomag3D_SSRTVD_NO_G_new \
+--mode 3D --model SSRTVD_NO_D \
+--data_folder Isomag3D --save_name isomag3D_SSRTVD_NO_D_new \
 --min_dimension_size 32 --cropping_resolution 96 \
 --num_blocks 5 --num_kernels 32 \
 --padding_mode reflect \
