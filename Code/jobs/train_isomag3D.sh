b2a4932cf5803236779dc74be7c54e2c170f1115
Skylar Wurster <swwurster@gmail.com>
2022-05-06 08:55:44 -0400

    Test for SSRTVD

--- Code/jobs/train_isomag3D.sh
+++ Code/jobs/train_isomag3D.sh
@@ -11,7 +11,7 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 #--epochs 20 
 
 python -u Code/train.py \
---train_distributed true --gpus_per_node 8 \
+--train_distributed false --gpus_per_node 8 \
 --mode 3D --model SSRTVD_NO_D \
 --data_folder Isomag3D --save_name isomag3D_SSRTVD_NO_D_new \
 --min_dimension_size 32 --cropping_resolution 96 \
