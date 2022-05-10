de057cd80806658cb9cb4d4455941f04a5faceb7
Skylar Wurster <swwurster@gmail.com>
2022-05-10 13:50:10 -0400

    Training hyperparameters tuned

--- Code/jobs/train_isomag2D.sh
+++ Code/jobs/train_isomag2D.sh
@@ -4,27 +4,33 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 python -u Code/train.py \
 --train_distributed true --gpus_per_node 8 \
 --mode 2D --model ESRGAN \
+--g_lr 0.0002 \
 --data_folder Isomag2D --save_name isomag2D_ESRGAN_new \
---min_dimension_size 32 --cropping_resolution 96 \
---num_blocks 5 --num_kernels 32 \
+--min_dimension_size 32 --cropping_resolution 256 \
+--num_blocks 3 --num_kernels 96 \
+--patch_size 1024 --training_patch_size 1024 \
 --padding_mode reflect \
---epochs 25 
+--epochs 50
 
 python -u Code/train.py \
 --train_distributed true --gpus_per_node 8 \
 --mode 2D --model STNet \
+--g_lr 0.0002 \
 --data_folder Isomag2D --save_name isomag2D_STNet_new \
---min_dimension_size 32 --cropping_resolution 96 \
---num_blocks 5 --num_kernels 32 \
+--min_dimension_size 32 --cropping_resolution 256 \
+--num_blocks 3 --num_kernels 96 \
+--patch_size 1024 --training_patch_size 1024 \
 --padding_mode reflect \
---epochs 25 
+--epochs 50 
 
 python -u Code/train.py \
 --train_distributed true --gpus_per_node 8 \
 --mode 2D --model SSRTVD_NO_D \
+--g_lr 0.0002 \
 --data_folder Isomag2D --save_name isomag2D_SSRTVD_NO_D_new \
---min_dimension_size 32 --cropping_resolution 96 \
---num_blocks 5 --num_kernels 32 \
+--min_dimension_size 32 --cropping_resolution 256 \
+--num_blocks 3 --num_kernels 96 \
+--patch_size 1024 --training_patch_size 1024 \
 --padding_mode reflect \
---epochs 25 
+--epochs 50 
 
