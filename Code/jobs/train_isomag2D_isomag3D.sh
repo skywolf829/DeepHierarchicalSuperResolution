ae5a11cb3cb50fefc797e063e7d0fa78be02873b
Skylar Wurster <swwurster@gmail.com>
2022-05-17 09:22:04 -0400

    Learning rate change

--- Code/jobs/train_isomag2D_isomag3D.sh
+++ Code/jobs/train_isomag2D_isomag3D.sh
@@ -4,7 +4,7 @@ cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 2D --model ESRGAN \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag2D --save_name isomag2D_ESRGAN_new \
 --min_dimension_size 32 --cropping_resolution 256 \
 --num_blocks 3 --num_kernels 96 \
@@ -15,7 +15,7 @@ python -u Code/train.py \
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 2D --model STNet \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag2D --save_name isomag2D_STNet_new \
 --min_dimension_size 32 --cropping_resolution 256 \
 --num_blocks 3 --num_kernels 96 \
@@ -26,7 +26,7 @@ python -u Code/train.py \
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 2D --model SSRTVD_NO_D \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag2D --save_name isomag2D_SSRTVD_NO_D_new \
 --min_dimension_size 32 --cropping_resolution 256 \
 --num_blocks 3 --num_kernels 96 \
@@ -37,7 +37,7 @@ python -u Code/train.py \
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 3D --model ESRGAN \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag3D --save_name isomag3D_ESRGAN_new \
 --min_dimension_size 32 --cropping_resolution 96 \
 --patch_size 96 --training_patch_size 96 \
@@ -49,7 +49,7 @@ python -u Code/train.py \
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 3D --model STNet \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag3D --save_name isomag3D_STNet_new \
 --min_dimension_size 32 --cropping_resolution 96 \
 --num_blocks 3 --num_kernels 96 \
@@ -60,7 +60,7 @@ python -u Code/train.py \
 python -u Code/train.py \
 --train_distributed false --gpus_per_node 8 \
 --mode 3D --model SSRTVD_NO_D \
---g_lr 0.0002 \
+--g_lr 0.00002 \
 --data_folder Isomag3D --save_name isomag3D_SSRTVD_NO_D_new \
 --min_dimension_size 32 --cropping_resolution 96 \
 --patch_size 96 --training_patch_size 96 \
